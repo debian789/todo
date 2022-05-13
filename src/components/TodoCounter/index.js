d316640bb6e81a2bc05678ece424df171fa76ed5
Miguel Suescun <debian789@gmail.com>
2022-05-13 07:39:14 -0500

    Implementacion de HOC

--- src/components/TodoCounter/index.js
+++ src/components/TodoCounter/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./TodoCounter.css";
 
+
+function contadorTotal(completedTodo, totalTodos) {
+  return function ContadorTodo(props) {
+    return (<span>{props.completedTodo} de {totalTodos}</span>)
+  }
+}
+
 function TodoCounter({ totalTodos, completedTodo, loading }) {
+  const WithContadorTotal = contadorTotal(completedTodo, totalTodos);
 
   return (
     <React.Fragment>
       <h1>Your Tasks</h1>
       <h4 className={!!loading ? 'title-loading' : undefined}>
-        Has completado {completedTodo} de {totalTodos} TODOs
+        Has completado <WithContadorTotal completedTodo = {completedTodo}/> TODOs
       </h4>
     </React.Fragment>
   );
